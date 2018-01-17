7cb446719b279b646771cdc288170726d290bcf1
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-01-17 15:35:55 -0800

    finished functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,38 @@
+//queue is thought of as "First in" is "first out"
+
 var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
+  var objcount = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    count ++;
+    objcount++;
+    // storage[count] = value;
+    storage[objcount] = value;
   };
 
   someInstance.dequeue = function() {
+    // var tempHolder = storage[count - count + 1];
+    var tempHolder = storage[objcount - count + 1];
+    // delete storage[count - count + 1];
+    delete storage[objcount - count + 1];
+    count --;
+    return tempHolder;
+
+    // need to alter code to remove properties from the front of an object
   };
 
   someInstance.size = function() {
+    if (count < 0) {
+      return 0;
+    } else {
+      return count;
+    }
   };
 
   return someInstance;
