1f54191859130ad87208b7c3477c4a12be4c0188
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-01-17 14:41:42 -0800

    finished functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,36 @@
+//stack is defined as "Last in" is the "first out"
+
 var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
   // Implement the methods below
+  // increasing
   someInstance.push = function(value) {
+  //need to accept non ints and remove in order they were added
+    count ++;
+    storage[count] = value;
   };
-
+  // decreasing
   someInstance.pop = function() {
+  //need to accept non ints and remove in order they were added 
+    var tempHolder = storage[count];
+    delete storage[count];
+    count --;
+    return tempHolder;
   };
-
+  // the size of something
   someInstance.size = function() {
+    if (count < 0) {
+      return 0;
+    } else {
+      return count;
+    }
+
+    //use object storage to store possible values
+    
   };
 
   return someInstance;
